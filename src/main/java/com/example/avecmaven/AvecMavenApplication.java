6acf75dd5802d191437bfcb378673999f42477b2
hamza znaidi <hamzaznaidi539@gmail.com>
2025-11-18 20:20:11 +0100

    commit1

---
+++ src/main/java/com/example/avecmaven/AvecMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.avecmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvecMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvecMavenApplication.class, args);
+    }
+
+}
