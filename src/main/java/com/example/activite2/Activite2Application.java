d215a98a27c96067be59cbee62e6aea7510d2071
hamza znaidi <hamzaznaidi539@gmail.com>
2025-11-18 23:26:11 +0100

    commit3

---
+++ src/main/java/com/example/activite2/Activite2Application.java
@@ -0,0 +1,13 @@
+package com.example.activite2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Activite2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Activite2Application.class, args);
+    }
+
+}
